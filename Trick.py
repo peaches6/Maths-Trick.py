97c325e45baf2bc698e61ad012f01b23fff7d207
peach, (Ai) <31016869+peaches6@users.noreply.github.com>
2017-10-16 21:49:39 +0100

    Convert input to lower : )
    Convert input to lower and validater string is in lower case, allows you to work with input without worrying about the case, basically not case-sensitive anymore

--- Trick.py
+++ Trick.py
@@ -1,34 +1,34 @@
 ready = input("This is a math trick. Are you ready? Y/N ")
 if ready == "Y":
   print("Ok. You will now begin the trick. Once you have each step worked out, type Ok.")
-  a = input("Think of a number between 1 and 10. ")
+  a = input("Think of a number between 1 and 10. ").lower()
 
 else:
   print("Well, go away then.")
   
 if a == "Ok":
   print("Next step")
-  b = input("Multiply that number by 2. ")
+  b = input("Multiply that number by 2. ").lower()
   
-if b == "Ok":
+if b == "ok":
   step1 = a * 2
   print("Next step")
-  c = input("Now, times that new number by 5. ")
+  c = input("Now, times that new number by 5. ").lower()
   
-if c == "Ok":
+if c == "ok":
   step2 = step1 * 5
   print("Next step")
-  d = input("Divide that new number by your original number. ")
+  d = input("Divide that new number by your original number. ").lower()
   
-if d == "Ok":
+if d == "ok":
   step3 = step2 / a
   print("Final step")
-  e = input("Subtract 7 from that number. ")
+  e = input("Subtract 7 from that number. ").lower()
   
-if e == "Ok":
+if e == "ok":
   print("Well done! Now I will guess your final answer.")
   step4 = step3 - 7
-  f = input("Is your answer 3? Y/N ")
+  f = input("Is your answer 3? Y/N ").lower()
   
 if f == "Y":
   print("Yay, I guessed your number!")
